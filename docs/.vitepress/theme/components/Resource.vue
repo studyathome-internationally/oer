4fba77a9a9bbcb2822aaad3fd04b177d70f5297b
Alija Sabic <sabic.alija@gmail.com>
2023-02-16 15:03:53 +0100

    Change external links (OERs) to open in the current tab

--- docs/.vitepress/theme/components/Resource.vue
+++ docs/.vitepress/theme/components/Resource.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <a :href="url" target="_blank" rel="noreferrer">
+    <a :href="url" target="_self" rel="noreferrer">
       <article class="resource">
         <h1>{{ title }}</h1>
         <img :src="src" :alt="alt" />
