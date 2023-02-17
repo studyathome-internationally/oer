b2fa49052d19a4393f8354a494a2dd027e67a91c
Alija Sabic <sabic.alija@gmail.com>
2023-02-17 14:46:41 +0100

    Add head configuration for favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vitepress";
+import { defineConfig, type HeadConfig } from "vitepress";
 import base from "./config/base.js";
 
 import MdItAbbr from "markdown-it-abbr";
 
 export default defineConfig({
   base,
+  head: head(),
   markdown: markdown(),
   locales: {
     root: {
@@ -38,6 +39,20 @@ export default defineConfig({
     },
   },
 });
+
+function head(): HeadConfig[] {
+  return [
+    ["link", { rel: "apple-touch-icon", sizes: "180x180", href: base + "favicon/apple-touch-icon.png" }],
+    ["link", { rel: "icon", type: "image/png", sizes: "32x32", href: base + "favicon/favicon-32x32.png" }],
+    ["link", { rel: "icon", type: "image/png", sizes: "16x16", href: base + "favicon/favicon-16x16.png" }],
+    ["link", { rel: "manifest", href: base + "favicon/site.webmanifest" }],
+    ["link", { rel: "mask-icon", href: base + "favicon/safari-pinned-tab.svg", color: "#00659c" }],
+    ["link", { rel: "shortcut icon", href: base + "favicon/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#ffffff" }],
+    ["meta", { name: "msapplication-config", content: base + "favicon/browserconfig.xml" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+  ];
+}
 
 function markdown() {
   return {
