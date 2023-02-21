584461767894980898fbbf1c8d7917911a107171
Alija Sabic <sabic.alija@gmail.com>
2023-02-21 15:02:11 +0100

    Revert "Disable pwa"
    This reverts commit d1eb99cdcf71d4b4ecefa672bdbf862208a89650.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,18 +1,18 @@
-// import { h } from "vue";
+import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import "./styles/index.styl";
 
 import Resources from "./components/Resources.vue";
 import Resource from "./components/Resource.vue";
-// import ReloadPrompt from "./components/ReloadPrompt.vue";
+import ReloadPrompt from "./components/ReloadPrompt.vue";
 
 export default {
   ...DefaultTheme,
-  // Layout() {
-  //   return h(DefaultTheme.Layout, null, {
-  //     "layout-bottom": () => h(ReloadPrompt),
-  //   });
-  // },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "layout-bottom": () => h(ReloadPrompt),
+    });
+  },
   enhanceApp(ctx) {
     // extend default theme custom behaviour.
     DefaultTheme.enhanceApp(ctx);
